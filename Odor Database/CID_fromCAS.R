6c44a55f466dffc45e4ed7163ef23df44641c3f4
Marissa Kamarck <marissa.kamarck@gmail.com>
2017-02-28 23:06:50 -0500

    added some analyses trying to figure out which chemical structures were correct

--- Odor Database/CID_fromCAS.R
+++ Odor Database/CID_fromCAS.R
@@ -71,25 +71,78 @@ pc_prop(cid, properties = NULL, verbose = TRUE, ...)
 # #write_csv(cid_smiles_density, "cid_smiles_density.csv")
 ###########
 #trying this with chemspider
-df <- read.csv("/Volumes/mainland/Projects/TAARs/Symrise\ -\ New\ Odors/Symrise\ Odor\ Database/ChemID_MW_SMILES_everything.csv")
+df <- read.csv("/Users/mkamarck/Documents/School\ and\ Lab/Mainland\ Lab/OdorDatabase/Symrise\ Odor\ Database/csid_compare.csv")
 df.test <- df[1:10,]
 
 # test = "100-06-1"
 # url = sprintf("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/xref/RegistryID/%s/JSON", test)
 
 add_cid = df %>>% rowwise() %>>% mutate(CID = get_cid_from_CAS(CAS)) %>>% ungroup()
+#write.csv(add_cid, "/Users/mkamarck/Documents/School\ and\ Lab/Mainland\ Lab/OdorDatabase/Symrise\ Odor\ Database/add_cid.csv")
 add_cid2 <- add_cid
 
 #get smiles from cid
-get_smiles_from_cid = function(CID) {
-  #MY_CID = get_cid(query = MY_SMILES, from = "smiles", first = T)
-  if (!is.na(CID)) {
-    out = pc_prop(cid = CID, properties = c("MolecularWeight", "CanonicalSMILES")) %>>%
-      mutate(input = SMILE, check = CanonicalSMILES == input)
-  } else {
-    out = data_frame(CID = NA, CanonicalSMILES = NA, check = NA)
+# get_smiles_from_cid = function(CID) {
+#   #MY_CID = get_cid(query = MY_SMILES, from = "smiles", first = T)
+#   if (!is.na(CID)) {
+#     out = pc_prop(cid = CID, properties = c("MolecularWeight", "CanonicalSMILES")) %>>%
+#       mutate(input = SMILE, check = CanonicalSMILES == input)
+#   } else {
+#     out = data_frame(CID = NA, CanonicalSMILES = NA, check = NA)
+#   }
+#   return(out)
+# }
+#df_forloop$test_cas[i] = cir_query(csid, 'cas', resolver = 'name_by_chemspider')
+#cid.next = df.test$CID %>>% map_df(get_smiles_from_cid)
+df.test = add_cid[1:10,]
+# df.test$smiles_fromCID = ""
+# df.test$MW_fromCID = ""
+# df.test$CID <- as.integer(df.test$CID)
+# df_forLoop = df.test
+# for (i in 1:length(df_forLoop)){
+#   cid = df_forLoop$CID[i]
+#   if (!is.na(cid)){
+#     print("yes")
+#     df_forLoop$smiles_fromCID[i] = pc_prop(cid, properties = "CanonicalSMILES")
+#   }
+# }
+
+get_smiles_from_CID = function(CID) {
+  if (is.na(CID)) {
+    out = "" #I changed this from NA without quotation marks
+  } 
+  else {
+    url = sprintf("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/%s/property/CanonicalSMILES/JSON", CID)
+    out = tryCatch({
+      url_got = GET(url, user_agent("myagent"))
+      source_PubChem = read_html(url_got) %>>% html_text()
+      smile = str_replace_all(source_PubChem, '\\"', '') %>>% 
+        str_split('CanonicalSMILES: ') %>>% unlist() %>>% 
+        map_chr(~ str_split(.x, "\n", n = 2) %>>% (.[[1]][1])) %>>% (.[2] )
+      return(smile)
+    }, 
+    error = function(e) {
+      cat("Error:", conditionMessage(e), "\n")
+      smile = ""
+      return(smile)
+    }, 
+    silent = TRUE
+    )
   }
   return(out)
 }
-#df_forloop$test_cas[i] = cir_query(csid, 'cas', resolver = 'name_by_chemspider')
-cid.next = df.test$CID %>>% map_df(get_smiles_from_cid)
+#url = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/1/property/CanonicalSMILES/JSON"
+add_smiles = add_cid %>>% rowwise() %>>% mutate(smiles_fromCID = get_smiles_from_CID(CID)) %>>% ungroup()
+
+
+
+
+#now take the original smiles from the chemspider and output more canonical smiles and CID using yusue's original code
+#Then check if these smiles match the canonical smiles
+#and see if the cids match the other match
+
+#for some reason none of these functions are working
+
+add_smiles$check= add_smiles$SMILE == add_smiles$smiles_fromCID
+
+write.csv(add_smiles, "checkThisByHand.csv")
